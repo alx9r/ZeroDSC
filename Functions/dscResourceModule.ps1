af6058ef062beae72f8f07038163694ac760dbbf
alx9r <eggy.island9@gmail.com>
2016-11-26 16:58:03 -1000

    Improve Public Appearance of Import-DscResource (#23)
    * add comment-based help to resolve #22
    * test that Import-DscResource dynamic keyword still works

--- Functions/dscResourceModule.ps1
+++ Functions/dscResourceModule.ps1
@@ -1,5 +1,21 @@
 function Import-DscResource
 {
+<#
+.SYNOPSIS
+Import a DSC resource for use in a ZeroDSC configuration document.
+
+Important: This is not the help for the built-in Import-DscResource dynamic keyword.
+
+.DESCRIPTION
+Import-DscResource imports a DSC resource.  It is meant for use only inside a ZeroDSC configuration document.  Import-DscResource has one parameter DscResource.  DscResource is type [DscResourceInfo] which the is the type emitted by Get-DscResource.  Import-DscResource creates an alias with the same name as the [DscResourceInfo] object.  That alias can then be used to declare resource configurations.
+
+Import-DscResource also emits DscResources to the pipeline.
+
+.OUTPUTS
+The object bound to DscResource.
+#>
+    [OutputType([Microsoft.PowerShell.DesiredStateConfiguration.DscResourceInfo])]
+    [CmdletBinding()]
     param
     (
         [Parameter(ValueFromPipeline = $true,
@@ -18,6 +34,7 @@ function Import-DscResource
 
 function Remove-DscResource
 {
+    [CmdletBinding()]
     param
     (
         [Parameter(ValueFromPipeline = $true,
