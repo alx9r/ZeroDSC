7b83bf05ca51e931d401ad52de75be5f78369ad7
alx9r <eggy.island9@gmail.com>
2016-12-24 08:59:26 -0800

    add test cases for .MoveNext() using simulated ConfigStep mention but leave commented-out tests for Invoke-ConfigStep

--- Functions/configInstructionsType.Tests.ps1
+++ Functions/configInstructionsType.Tests.ps1
@@ -91,6 +91,49 @@ $tests = [ordered]@{
             @($null,            ,'Ended')
         )
     }
+    'Exception during Pretest' = @{
+        ConfigDocument = {
+            Get-DscResource StubResource5 | Import-DscResource
+            StubResource5 'a' @{ Mode = 'Normal'; DependsOn = '[StubResource5]b' }
+            StubResource5 'b' @{ Mode = 'Normal' }
+        }
+        Values = @(
+            # NodeKey           | StateName                    | Node Progress | Raise Event
+            @('[StubResource5]a','PretestWaitForTestExternal',  'Complete',      'TestCompleteSuccess' ),
+            @('[StubResource5]b','PretestWaitForTestExternal',  'Exception',     'TestThrew' ),
+            @($null,            ,'Ended' )
+        )
+    }
+    'Exception during Configure Test' = @{
+        ConfigDocument = {
+            Get-DscResource StubResource5 | Import-DscResource
+            StubResource5 'a' @{ Mode = 'Normal'; DependsOn = '[StubResource5]b' }
+            StubResource5 'b' @{ Mode = 'Normal' }
+        }
+        Values = @(
+            # NodeKey           | StateName                    | Node Progress | Raise Event
+            @('[StubResource5]a','PretestWaitForTestExternal',  'Complete',      'TestCompleteSuccess' ),
+            @('[StubResource5]b','PretestWaitForTestExternal',  $null,           'TestCompleteFailure' ),
+            @('[StubResource5]b','ConfigureWaitForSetExternal', $null,           'SetComplete' ),
+            @('[StubResource5]b','ConfigureWaitForTestExternal','Exception',     'TestThrew'),
+            @($null,            ,'Ended' )
+        )
+    }
+    'Exception during Configure Set' = @{
+        ConfigDocument = {
+            Get-DscResource StubResource5 | Import-DscResource
+            StubResource5 'a' @{ Mode = 'Normal'; DependsOn = '[StubResource5]b' }
+            StubResource5 'b' @{ Mode = 'Normal' }
+        }
+        Values = @(
+            # NodeKey           | StateName                    | Node Progress | Raise Event
+            @('[StubResource5]a','PretestWaitForTestExternal',  'Complete',      'TestCompleteSuccess' ),
+            @('[StubResource5]b','PretestWaitForTestExternal',  $null,           'TestCompleteFailure' ),
+            @('[StubResource5]b','ConfigureWaitForSetExternal', 'Exception',      'SetThrew' ),
+            @('[StubResource5]b','ConfigureWaitForTestExternal','Complete',      'TestCompleteSuccess'),
+            @($null,            ,'Ended' )
+        )
+    }
     'Skip All' = @{
         ConfigDocument = {
             Get-DscResource StubResource5 | Import-DscResource
@@ -298,16 +341,18 @@ Describe 'Get-CurrentConfigStep' {
     }
 }
 
-
 Describe 'Invoke-ConfigStep' {
     $h = @{}
     Context 'arrange' {
         It 'create test document' {
             $h.doc = New-RawConfigDocument Name {
                 Get-DscResource StubResource5 | Import-DscResource
+#                Get-DscResource TestStub | Import-DscResource
                 StubResource5 'a' @{ Mode = 'already set' }
                 StubResource5 'b' @{ Mode = 'normal' }
                 StubResource5 'c' @{ Mode = 'incorrigible' }
+#                TestStub 'd' @{ Key = 'd'; ThrowOnTest = 'always' }
+#                TestStub 'e' @{ Key = 'e'; ThrowOnSet = 'always' }
             } |
                 ConvertTo-ConfigDocument
         }
@@ -321,10 +366,14 @@ Describe 'Invoke-ConfigStep' {
             @('[StubResource5]a','PretestWaitForTestExternal',          'TestCompleteSuccess',  'Pending',       'Complete',      $true,  'Test', 'PreTest' ),
             @('[StubResource5]b','PretestWaitForTestExternal',          'TestCompleteFailure',  'Pending',       'Pending',       $false, 'Test', 'PreTest' ),
             @('[StubResource5]c','PretestWaitForTestExternal',          'TestCompleteFailure',  'Pending',       'Pending',       $false, 'Test', 'PreTest' ),
+#            @('[TestStub]d',     'PretestWaitForTestExternal',          'TestThrew',            'Pending',       'Exception',     $null,  'Test', 'PreTest' ),
+#            @('[TestStub]e',     'PretestWaitForTestExternal',          'TestCompleteFailure',  'Pending',       'Pending',       $false, 'Test', 'PreTest' ),
             @('[StubResource5]b','ConfigureWaitForSetExternal',         'SetComplete',          'Pending',       'Pending',       $null,  'Set',  'Configure' ),
             @('[StubResource5]b','ConfigureWaitForTestExternal',        'TestCompleteSuccess',  'Pending',       'Complete',      $true,  'Test', 'Configure' ),
             @('[StubResource5]c','ConfigureProgressWaitForSetExternal', 'SetComplete',          'Pending',       'Pending',       $null,  'Set',  'Configure' ),
             @('[StubResource5]c','ConfigureProgressWaitForTestExternal','TestCompleteFailure',  'Pending',       'Failed',        $false, 'Test', 'Configure' ),
+#            @('[TestStub]e',     'ConfigureProgressWaitForSetExternal', 'SetThrew',             'Pending',       'Exception',     $null,  'Set',  'Configure' ),
+#            @('[TestStub]e',     'ConfigureProgressWaitForTestExternal','TestCompleteSuccess',  'Pending',       'Complete',      $false, 'Test', 'Configure' ),
             @($null,            ,'Ended')
         )
     )
