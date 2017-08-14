ed1717c636cf26a47ebc2e7a68fd991ea8bc9a1e
alx9r <eggy.island9@gmail.com>
2017-08-14 09:10:45 -0700

    accommodate duplicate instances of ZeroDsc on host #44
    use currently-loaded version of ZeroDsc for test

--- Functions/dscResourceModulePublic.Tests.ps1
+++ Functions/dscResourceModulePublic.Tests.ps1
@@ -3,14 +3,16 @@ Import-Module PSDesiredStateConfiguration
 
 Describe 'Import-DscResource dynamic keyword still works' {
     It 'build configuration artifacts' {
+        Invoke-Expression @"
         configuration TestConfiguration
         {
-            Import-DscResource -Name TestStub -ModuleName ZeroDSC
+            Import-DscResource -Name TestStub -ModuleName ZeroDSC -ModuleVersion $((Get-Module ZeroDsc).Version)
             TestStub a
             {
                 Key = 'a'
             }
         }
+"@
 
         $r = TestConfiguration -OutputPath ([System.IO.Path]::GetTempPath())
         $r | Should not beNullOrEmpty
